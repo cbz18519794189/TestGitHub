dce7fbf554739c538016175b4bf249008500ebd7
Cbz_ziiz <cbz18519794189@gmail.com>
2017-02-18 23:17:24 +0800

    QQ

---
+++ QQ.java
@@ -0,0 +1,17 @@
+public class Test
+{
+	public static void main(String[] args) 
+	{
+		int[] num = new int[]{0,1,3,5,7,8};
+		int[] arr = new int[]{1,0,4,5,3,2,0,4,1,2};
+		String ziiz = "";
+		for (int i : arr)
+		 {
+			ziiz += num[i];
+		}
+		System.out.println(ziiz);
+		System.out.println("手头有SSM或SSH项目的大腿求带飞一波");
+	}
+}
+
+	
